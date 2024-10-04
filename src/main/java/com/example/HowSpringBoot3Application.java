39bda8b2dc777cb54c235bc752e004544075954c
cbq1024 <caobq@gmail.com>
2024-10-04 19:12:42 +0800

    init project

---
+++ src/main/java/com/example/HowSpringBoot3Application.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HowSpringBoot3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HowSpringBoot3Application.class, args);
+    }
+
+}
